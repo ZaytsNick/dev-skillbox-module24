5a8628a3f315e418cc4145da4b152aba21c588e8
zayts_home <10kcin01@gmail.com>
2024-03-24 22:14:26 +0500

    task#3

---
+++ Task#3/main3.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <ctime>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <map>
+
+int main()
+{
+    std::time_t t = std::time(nullptr);
+    std::cout << "Enter minutes and seconds: ";
+    std::tm temp = *std::localtime(&t);
+    std::cin >> std::get_time(&temp, "%M:%S");
+    t = std::time(nullptr);
+    std::tm end_timer = *std::localtime(&t);
+    end_timer.tm_min = (end_timer.tm_min + temp.tm_min) % 60 + (end_timer.tm_sec + temp.tm_sec) / 60;
+    end_timer.tm_sec = (end_timer.tm_sec + temp.tm_sec) % 60;
+    // temp = *std::localtime(&t);
+    // std::tm start_timer = temp;
+    while ((temp.tm_min != end_timer.tm_min) || (temp.tm_sec != end_timer.tm_sec))
+    {
+        t = std::time(nullptr);
+        temp = *std::localtime(&t);
+        int second = end_timer.tm_sec - temp.tm_sec < 0 ? end_timer.tm_sec - temp.tm_sec + 60 : end_timer.tm_sec - temp.tm_sec;
+        int minute = end_timer.tm_min - (end_timer.tm_sec - temp.tm_sec < 0 ? 1 : 0) - temp.tm_min;
+        std::cout << minute << ":" << second << std::endl;
+    }
+    // std::cout << std::put_time(&end_timer, "%M:%S") << " " << std::put_time(&start_timer, "%M:%S");
+    std::cout << "DING! DING! DING!";
+}
